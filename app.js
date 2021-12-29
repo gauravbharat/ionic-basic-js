abb11de6df9e3ca0a235eebe83e0d0933e273dd3
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-12-29 11:54:01 +0530

    show total and alert

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ const amountInput = document.querySelector("#input-amount");
 const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
 const expensesList = document.querySelector("#expenses-list");
+const totalExpensesOutput = document.querySelector("#total-expenses");
+
+// const alertCtrl = document.querySelector("ion-alert-controller");
+
+let totalExpenses = 0;
 
 const clear = () => {
   reasonInput.value = "";
   amountInput.value = "";
 };
+
+async function presentAlert(props) {
+  const alert = document.createElement("ion-alert");
+  // alert.cssClass = 'my-custom-class';
+  alert.header = props.header;
+  // alert.subHeader = 'Subtitle';
+  alert.message = props.message;
+  alert.buttons = props.buttons;
+
+  document.body.appendChild(alert);
+  await alert.present();
+
+  const { role } = await alert.onDidDismiss();
+  console.log("onDidDismiss resolved with role", role);
+}
 
 const confirmBtnSub = rxjs.fromEvent(confirmBtn, "click").subscribe((event) => {
   const enteredReason = reasonInput.value;
@@ -18,6 +38,21 @@ const confirmBtnSub = rxjs.fromEvent(confirmBtn, "click").subscribe((event) => {
     enteredAmount <= 0 ||
     enteredAmount.trim().length <= 0
   ) {
+    presentAlert({
+      message: "Please enter a valid reason and amount!",
+      header: "Invalid inputs",
+      buttons: ["Okay"],
+    });
+
+    // alertCtrl
+    //   .create({
+    //     message: "Please enter a valid reason and amount!",
+    //     header: "Invalid inputs",
+    //     buttons: ["Okay"],
+    //   })
+    //   .then((alertElement) => {
+    //     alertElement.present();
+    //   });
     return;
   }
 
@@ -27,6 +62,9 @@ const confirmBtnSub = rxjs.fromEvent(confirmBtn, "click").subscribe((event) => {
   newItem.textContent = `${enteredReason}: \$${enteredAmount}`;
 
   expensesList.appendChild(newItem);
+
+  totalExpenses += +enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses;
 
   clear();
 });
